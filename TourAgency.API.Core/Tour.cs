6addb622e3dbb136ccbb0ee78caea1fcdb5d0372
Barbyao <varvara2203@gmail.com>
2024-02-11 20:32:00 +0200

    в tour.cs добавила расчет прайса Web новый проект

--- TourAgency.API.Core/Tour.cs
+++ TourAgency.API.Core/Tour.cs
@@ -1,4 +1,6 @@
-﻿namespace TourAgency.API.Core
+﻿using System.Diagnostics;
+
+namespace TourAgency.API.Core
 {
     public class Tour
     {
@@ -6,5 +8,36 @@
         {
 
         }
-    }
+        
+    
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+
+            public Tour()
+            {
+                // Конструктор без параметров
+            }
+
+            public Tour(string name, string description, decimal price)
+            {
+                // Конструктор с параметрами
+                Name = name;
+                Description = description;
+                Price = price;
+            }
+
+            
+
+            public decimal CalculateTotalCost(int numberOfPeople)
+            {
+                // Реализация логики для расчета общей стоимости тура в зависимости от количества человек
+                return Price * numberOfPeople;
+            }
+
+            // Другие методы обработки данных
+        
+    
+
+}
 }
